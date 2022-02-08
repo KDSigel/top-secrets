8d9bfa28b7c366f11b229369b1905fee185c3cab
KDSigel <heykarl@gmail.com>
2022-02-08 12:54:32 -0800

    added a sign out user test but unsure if I wrote it correctly. Copy/pasted registerAndLogin function from Dan's code because I was told to

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,10 +2,27 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const fakeUser = {
   email: 'broken@arrow.com',
   password: 'Nucflash'
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? fakeUser.password;
+
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
+
+  // Create a user to sign in with
+  const user = await UserService.create({ ...fakeUser, ...userProps });
+
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('backend routes', () => {
@@ -31,6 +48,12 @@ describe('backend routes', () => {
     expect(response.body).toEqual({
       message: 'you signed in, great job!'
     });
+  });
+
+  it('should sign out a user', async () => {
+    const [agent, user] = await registerAndLogin();
+    await agent.delete('/api/v1/users/sessions');
+    expect(user).toEqual(null);
   });
 
 });
